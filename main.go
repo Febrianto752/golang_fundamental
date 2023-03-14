9dc6974d43d7acc422fa3f307e92a2195a00db79
Febrianto <febrianto.bekasi@gmail.com>
2023-03-14 19:23:43 +0700

    update loop russian unicode

--- main.go
+++ main.go
@@ -2,13 +2,9 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
 func main() {
-	unicodesValues := [7]string{"\u0421", "\u0410", "\u0428", "\u0410", "\u0420", "\u0412", "\u041E"}
-	unicodesString := [7]string{"U+0421", "U+0410", "U+0428", "U+0410", "U+0420", "U+0412", "U+041E"}
-
 	for i := 0; i <= 5; i++ {
 		if i < 5 {
 			fmt.Println("Nilai i =", i)
@@ -16,10 +12,9 @@ func main() {
 		} else {
 			for j := 0; j <= 10; j++ {
 				if j == 5 {
-					var position int64 = 0
-					for index, unicode := range unicodesValues {
-						fmt.Println("character " + unicodesString[index] + " '" + unicode + "' starts at byte position " + strconv.FormatInt(position, 10))
-						position += 2
+					const RussianUnicodes = "САШАРВО"
+					for index, value := range RussianUnicodes {
+						fmt.Printf("character %U '%c' starts at byte position %d\n", value, value, index)
 					}
 				} else {
 					fmt.Println("Nilai j =", j)
